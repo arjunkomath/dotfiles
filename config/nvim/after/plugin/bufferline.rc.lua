fa60eb989cbc39e245c4e6afcf3f7aed4de665b8
Arjun Komath <arjun@hey.com>
2023-05-27 23:02:32 +1000

    Match bufferline colors to theme

--- config/nvim/after/plugin/bufferline.rc.lua
+++ config/nvim/after/plugin/bufferline.rc.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
+    -- separator_style = 'slant',
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
@@ -12,22 +12,22 @@ bufferline.setup({
   },
   highlights = {
     separator = {
-      fg = '#073642',
-      bg = '#002b36',
+      fg = '#403d52',
+      bg = '#908caa',
     },
     separator_selected = {
-      fg = '#073642',
+      fg = '#403d52',
     },
     background = {
-      fg = '#657b83',
-      bg = '#002b36'
+      fg = '#26233a',
+      bg = '#908caa'
     },
     buffer_selected = {
       fg = '#fdf6e3',
       bold = true,
     },
     fill = {
-      bg = '#073642'
+      bg = '#403d52'
     }
   },
 })
