ffc507ce25b05004bd7eeb4a34aab6a1f4c9971d
Arjun Komath <arjun@hey.com>
2023-06-24 09:41:34 +1000

    Add few more apps

--- config/nvim/after/plugin/bufferline.rc.lua
+++ config/nvim/after/plugin/bufferline.rc.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
+    -- separator_style = 'slant',
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
