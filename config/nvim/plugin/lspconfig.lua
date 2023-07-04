164f431cc78a781a4e1ee2b7ac9953df4ae92205
Arjun Komath <arjun@hey.com>
2023-07-04 20:10:35 +1000

    Fix indentation

--- config/nvim/plugin/lspconfig.lua
+++ config/nvim/plugin/lspconfig.lua
@@ -136,11 +136,11 @@ rt.setup()
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = "●" },
-  severity_sort = true,
-}
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = "●" },
+    severity_sort = true,
+  }
 )
 
 -- Diagnostic symbols in the sign column (gutter)
