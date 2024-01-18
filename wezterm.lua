30273ddd6bf70ee55caed47301719707872b4411
Arjun Komath <arjunkomath@gmail.com>
2024-01-19 08:21:13 +1100

    Update tmux

--- wezterm.lua
+++ wezterm.lua
@@ -6,7 +6,16 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Dark Pastel'
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+custom.background = "#000000"
+custom.tab_bar.background = "#040404"
+custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+custom.tab_bar.new_tab.bg_color = "#080808"
+
+config.color_schemes = {
+  ["OLEDppuccin"] = custom,
+}
+config.color_scheme = "OLEDppuccin"
 
 -- config.window_decorations = 'INTEGRATED_BUTTONS'
 config.window_decorations = 'RESIZE'
