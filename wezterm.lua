4336bf6c7b509174c3dadb0364d22939442efc6c
Arjun Komath <arjunkomath@gmail.com>
2024-07-16 15:24:51 +1000

    Rose pine for wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -7,16 +7,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
-custom.background = "#000000"
-custom.tab_bar.background = "#040404"
-custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
-custom.tab_bar.new_tab.bg_color = "#080808"
-
-config.color_schemes = {
-  ["OLEDppuccin"] = custom,
-}
-config.color_scheme = "OLEDppuccin"
+config.color_scheme = 'rose-pine'
 
 -- config.window_decorations = 'INTEGRATED_BUTTONS'
 config.window_decorations = 'RESIZE'
