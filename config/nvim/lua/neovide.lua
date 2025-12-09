4e5e904cab113097f550d1a40b7103e56db6a86b
Arjun Komath <arjunkomath@gmail.com>
2025-12-09 23:14:57 +1100

    clean up neovide

--- config/nvim/lua/neovide.lua
+++ config/nvim/lua/neovide.lua
@@ -1,4 +1,2 @@
 vim.g.neovide_cursor_animate_command_line = false
-vim.o.guifont = "Fira Code Retina:h16:#e-subpixelantialias"
-vim.g.neovide_cursor_animate_command_line = false
-vim.g.neovide_line_height = 1.2
+vim.o.guifont = "Fira Code Retina:h17"
