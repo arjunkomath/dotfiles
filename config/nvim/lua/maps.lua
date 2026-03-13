068e0100dceead06534a8b27081582be475f6924
Arjun Komath <arjunkomath@gmail.com>
2026-03-13 16:35:45 +1100

    Remove split keymaps

--- config/nvim/lua/maps.lua
+++ config/nvim/lua/maps.lua
@@ -6,10 +6,6 @@ keymap.set('n', 'x', '"_x')
 keymap.set('n', '<C-s>', ':w<Return>')
 
 
--- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
-
 -- Move lines
 keymap.set('n', '<C-j>', ':m .+1<CR>==')
 keymap.set('n', '<C-k>', ':m .-2<CR>==')
